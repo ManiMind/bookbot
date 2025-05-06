2b9796a1a32cba16cff8c493a0a8aa724c51c793
ManiMind <rolf.a.bork@outlook.com>
2025-05-06 17:06:40 +0200

    added main() and functions for returning file as a string, and counting words of a string

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    words = get_word_count(text)
+    print(f"{words} words found in the document")    
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def get_word_count(text):
+    words = text.split()
+    count = len(words)
+    return count
+
+main()
