58d78ab978e21174dfc2bcb4edb5e0bca0a551b5
ManiMind <rolf.a.bork@outlook.com>
2025-05-06 17:52:30 +0200

    added function for counting characters and returning them in a dictionary, and called that function in main

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_characters
 
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     words = get_num_words(text)
-    print(f"{words} words found in the document")    
+    characters = get_num_characters(text)
+    print(f"{words} words found in the document")
+    print(characters)
+
 
 
 def get_book_text(path):
--- stats.py
+++ stats.py
@@ -2,3 +2,17 @@ def get_num_words(text):
     words = text.split()
     count = len(words)
     return count
+
+def get_num_characters(text):
+    # empty dictionary for storing the counts
+    letter_nums = {}
+
+    # loop through each character in the text and add or increment them    
+    for character in text:
+        character = character.lower()
+        if character in letter_nums:
+            letter_nums[character] += 1
+        else:
+            letter_nums[character] = 1
+        
+    return letter_nums
