fc8e11b61297db45907354f0e021f81f7b342928
ManiMind <rolf.a.bork@outlook.com>
2025-05-06 17:12:49 +0200

    refactored by moving get_num_words function to a seperate file and importing it in main

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
+from stats import get_num_words
+
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    words = get_word_count(text)
+    words = get_num_words(text)
     print(f"{words} words found in the document")    
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 
-def get_word_count(text):
-    words = text.split()
-    count = len(words)
-    return count
+
+
+
 
 main()
