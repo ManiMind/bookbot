0f91f72e8b8a557d6213f4477a9f25cfb9f40a23
ManiMind <rolf.a.bork@outlook.com>
2025-05-07 11:55:32 +0200

    added if statement checking for arguments, and changed book_path to use the argument from position 1

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from stats import get_num_words, get_num_characters, chars_dict_to_sorted_list
+import sys
 
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     words = get_num_words(text)
     characters = get_num_characters(text)
